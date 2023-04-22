dcf0a10da8c9e5f508b95fa9d83fdb3287937e43
NicolasMillar <nicolas.millar1601@alumnos.ubiobio.cl>
2023-04-22 18:03:05 -0400

    api de login para admin deberia estar lista

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -17,6 +17,25 @@ class Home extends BaseController
 
         if($userType === 'administrador'){
             $admin = new usuarioAdmin();
+
+            $dataUsuario = $admin->obtenerAdmin(['rut' => $rut]);
+
+            if(count($dataUsuario) > 0 && password_verify($password, $dataUsuario[0]['Constraseña'])){
+                $token = generateToken($dataUsuario[0]['Rut'], $userType);
+                return $response->setJSON(['token' => $token]);
+            }else{
+                return $response->setStatusCode(401)->setJSON(['error' => 'Credenciales inválidas']);
+            }
         }
+    }
+
+    function generateToken($userId, $userType) {
+        $randomString = bin2hex(random_bytes(16));
+        $data = ['userId' => $userId, 'userType' => $userType];
+        $jsonData = json_encode($data);
+        $signature = hash_hmac('sha256', $jsonData, $randomString);
+        $token = $randomString . '.' . $signature;
+        
+        return $token;
     }
 }
