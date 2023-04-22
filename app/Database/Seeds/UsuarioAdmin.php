e2b80d5190c7c33822954deaf536a933f932573b
NicolasMillar <nicolas.millar1601@alumnos.ubiobio.cl>
2023-04-22 17:15:20 -0400

    seed de admin

---
+++ app/Database/Seeds/UsuarioAdmin.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class UsuarioAdmin extends Seeder
+{
+    public function run()
+    {
+        $password = password_hash("123", PASSWORD_DEFAULT);
+
+        $data = [
+            'Rut' => '198347442',
+            'Nombre' => 'Nicolás Alejandro',
+            'ApellidoM' => 'Millar',
+            'ApellidoP' => 'Millar',
+            'Constraseña' => $password
+        ];
+
+        $this->db->table('usuarioAdmin')->insert($data);
+    }
+}
