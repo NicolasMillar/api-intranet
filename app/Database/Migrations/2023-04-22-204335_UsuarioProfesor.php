60f7d37b27b9f45a02f4ffaa84a5da4ccba7a682
NicolasMillar <nicolas.millar1601@alumnos.ubiobio.cl>
2023-04-22 16:57:41 -0400

    creadas las migraciones

---
+++ app/Database/Migrations/2023-04-22-204335_UsuarioProfesor.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class UsuarioProfesor extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'Rut' => [
+                'type'           => 'Varchar',
+                'constraint'     => '19',
+                'unsigned'       => true,
+            ],
+            'Nombre' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'ApellidoM' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'ApellidoP' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'Constraseña' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'Direccion' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'Comuna' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'Region' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'FechaNacimiento' => [
+                'type'       => 'Date',
+            ],
+            'FechaIngreso' => [
+                'type'       => 'Date',
+            ],
+            'Imagen' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+        ]);
+        $this->forge->addKey('Rut', true);
+        $this->forge->createTable('usuarioProfesor');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('usuarioProfesor');
+    }
+}
---
+++ app/Database/Migrations/2023-04-22-204342_UsuarioAdmin.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class UsuarioAdmin extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'Rut' => [
+                'type'           => 'Varchar',
+                'constraint'     => '19',
+                'unsigned'       => true,
+            ],
+            'Nombre' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'ApellidoM' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'ApellidoP' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'Constraseña' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+        ]);
+        $this->forge->addKey('Rut', true);
+        $this->forge->createTable('usuarioAdmin');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('usuarioAdmin');
+    }
+}
