8d748494fbc538e7c8e31fa40a7bb87e97918bf5
NicolasMillar <nicolas.millar1601@alumnos.ubiobio.cl>
2023-04-24 01:24:22 -0400

    cambios pequeños intentando que funcione en azure

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -31,7 +31,6 @@ $routes->set404Override();
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
 $routes->post('/login', 'Home::login');
-$route->post('/prueba', 'Home::prueba');
 
 /*
  * --------------------------------------------------------------------
