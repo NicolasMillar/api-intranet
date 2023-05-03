168a85aeb3e2f240bc125f942baeb1a02e735a1b
NicolasMillar <nicolas.millar1601@alumnos.ubiobio.cl>
2023-05-03 15:39:28 -0400

    terminada la configuracion de los cors

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -21,6 +21,7 @@ class Filters extends BaseConfig
         'honeypot'      => Honeypot::class,
         'invalidchars'  => InvalidChars::class,
         'secureheaders' => SecureHeaders::class,
+        'cors' => \Fluent\Cors\Filters\CorsFilter::class,
     ];
 
     /**
@@ -60,5 +61,8 @@ class Filters extends BaseConfig
      * Example:
      * 'isLoggedIn' => ['before' => ['account/*', 'profiles/*']]
      */
-    public array $filters = [];
+    public array $filters = ['cors' => [
+        'before' => ['/*'],
+        'after' => ['/*']
+    ],];
 }
